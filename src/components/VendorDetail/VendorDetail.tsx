b5de09cf9cde9b0b739f94a4d5b885f7f827e862
Šimon <simonbuchta2005@gmail.com>
2024-03-12 09:11:41 +0100

    faq test

--- src/components/VendorDetail/VendorDetail.tsx
+++ src/components/VendorDetail/VendorDetail.tsx
@@ -192,7 +192,8 @@ const VendorDetail: React.FC<VendorDetailProps> = ({ vendor, user, sendStats }) 
         </Flex>
       </Card>
       {currentDescription && <VendorDescription desc={currentDescription} />}
-      {(vendor.faq.length !== 0 && vendor.isPremium) && <FAQ vendor={vendor} />}
+      {/* {(vendor.faq.length !== 0 && vendor.isPremium) && <FAQ vendor={vendor} />} */}
+      <FAQ vendor={vendor} />
       <DealsCard vendor={vendor} />
       <VendorLocation vendor={vendor} />
       <ReviewsCard vendorId={vendor.id}/>
