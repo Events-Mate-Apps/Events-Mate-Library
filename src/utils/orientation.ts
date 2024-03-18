ee372d33e21f902e7ab04d2023bb50ed275e8fdc
Šimon <simonbuchta2005@gmail.com>
2024-03-18 19:28:30 +0100

    isEventsMate fi

--- src/utils/orientation.ts
+++ src/utils/orientation.ts
@@ -2,5 +2,5 @@ import store from 'store'
 
 export const isEventsMate = (): boolean => {
     const IS_EVENTS_MATE = store.get('IS_EVENTS_MATE');
-    return JSON.parse(IS_EVENTS_MATE);
+    return IS_EVENTS_MATE ? true : false;
 };
