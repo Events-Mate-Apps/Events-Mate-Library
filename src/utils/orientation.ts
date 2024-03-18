749e781dd32d6bfaf492e9b26bbfe06e55918443
Šimon <simonbuchta2005@gmail.com>
2024-03-18 19:54:38 +0100

    new mechanism

--- src/utils/orientation.ts
+++ src/utils/orientation.ts
@@ -1,6 +1,7 @@
-import store from 'store'
-
-export const isEventsMate = (): boolean => {
-    const IS_EVENTS_MATE = store.get('IS_EVENTS_MATE');
-    return IS_EVENTS_MATE ? true : false;
-};
+export const isEventsMate = (): boolean | null => {
+    const metaTag: HTMLMetaElement | null = document.querySelector('meta[property="og:site_name"]');
+    console.log(metaTag, 'metaTag')
+    console.log(metaTag?.content, 'content')
+    if (!metaTag) return null
+    return metaTag.content === 'Events-Mate'
+}
