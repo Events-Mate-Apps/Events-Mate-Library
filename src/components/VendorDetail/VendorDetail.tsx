1a88428952fe9d8e204783bb8fd79da777805c6d
Šimon <simonbuchta2005@gmail.com>
2024-03-29 12:38:11 +0100

    isEM

--- src/components/VendorDetail/VendorDetail.tsx
+++ src/components/VendorDetail/VendorDetail.tsx
@@ -196,6 +196,7 @@ const VendorDetail: React.FC<VendorDetailProps> = ({ vendor, user, sendStats }) 
       <DealsCard vendor={vendor} />
       <VendorLocation vendor={vendor} />
       <ReviewsCard vendorId={vendor.id}/>
+      <Text color={isEventsMate() ? 'blue.900' : 'red.900'}>color</Text>
     </Flex>
   );
 }
