d5aba87a0b876dafae3ca79191c6ab545d4894b6
Šimon <simonbuchta2005@gmail.com>
2024-03-29 12:40:05 +0100

    bug fix commit

--- src/utils/orientation.ts
+++ src/utils/orientation.ts
@@ -1,4 +1,5 @@
 export const isEventsMate = (): boolean | null => {
+    if (!document) return null
     const metaTag: HTMLMetaElement | null = document.querySelector('meta[property="og:site_name"]');
     if (!metaTag) return null
     return metaTag.content === 'Events-Mate'
