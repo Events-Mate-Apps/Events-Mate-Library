bbe239f07205d0ffe50b425b282e5c5a2e37c656
Šimon <simonbuchta2005@gmail.com>
2024-04-06 17:52:47 +0200

    merging locales services

--- src/service/LocalizationService.ts
+++ src/service/LocalizationService.ts
@@ -3,7 +3,8 @@ import { useMemo } from "react";
 import { TranslationTextContent } from "../interfaces/vendor";
 
 interface LocalizationMethods {
-    getCurrentTranslation: (content: TranslationTextContent, language?: string) => string;
+    getCurrentTranslation: (content: TranslationTextContent, language?: string) => string,
+    extractLanguageISOCodesFromObject: (obj: object) => string[]
 }
 
 export function useLocalization(): LocalizationMethods {
@@ -19,7 +20,27 @@ export function useLocalization(): LocalizationMethods {
         return translation
     }
 
+    function extractLanguageISOCodesFromObject(obj: object): string[] {
+        let langs: string[] = [];
+      
+        function extractLanguages(obj: any) {
+          if (obj !== null && typeof obj === 'object') {
+            Object.entries(obj).forEach(([key, value]) => {
+              if (key === 'languageISO' && typeof value === 'string') {
+                !langs.includes(value) && langs.push(value);
+              } else if (typeof value === 'object') {
+                extractLanguages(value);
+              }
+            });
+          }
+        }
+      
+        extractLanguages(obj);
+        return langs;
+    }
+
     return {
-        getCurrentTranslation
+        getCurrentTranslation,
+        extractLanguageISOCodesFromObject
     }
 }
