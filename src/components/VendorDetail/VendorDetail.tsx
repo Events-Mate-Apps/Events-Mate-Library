be727e89e95ce5380ecc3292728628ab94cc9e49
Šimon <simonbuchta2005@gmail.com>
2024-04-12 12:49:22 +0200

    test

--- src/components/VendorDetail/VendorDetail.tsx
+++ src/components/VendorDetail/VendorDetail.tsx
@@ -144,11 +144,11 @@ const VendorDetail: React.FC<VendorDetailProps> = ({ vendor, user, sendStats }) 
                                 >
                                     {vendor.name}
                                 </Text>
-                                {FontAwesomeIconWrapper && <FontAwesomeIconWrapper 
+                                <FontAwesomeIconWrapper 
                                     iconString='fa-solid fa-crown'
                                     color='red'
                                     size='xs'
-                                />}
+                                />
                             </Flex>
                             <ReviewStars
                                 score={vendor.rating}
