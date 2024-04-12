ecec9ccdf45b95348a8799b808f5e8ad1440aac5
Šimon <simonbuchta2005@gmail.com>
2024-04-12 12:30:09 +0200

    async icon test

--- src/components/FontAwesomeIconWrapper.tsx
+++ src/components/FontAwesomeIconWrapper.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp, SizeProp, library } from '@fortawesome/fontawesome-svg-core';
+import { IconProp, SizeProp, findIconDefinition, library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fal } from '@fortawesome/pro-light-svg-icons';
 import { fas } from '@fortawesome/pro-solid-svg-icons';
 import { FC, memo } from 'react';
+
 library.add(fas, far, fal);
 
 interface FAWrapperProps {
@@ -15,24 +16,23 @@ interface FAWrapperProps {
 
 const FontAwesomeIconWrapper: FC<FAWrapperProps> = ({ icon, color, size }) => {
   const [loading, setLoading] = useState(true);
+  const [iconExists, setIconExists] = useState(true);
 
   useEffect(() => {
-    const checkIcon = async () => {
-      try {
-        // Simulate async loading of icon
-        await new Promise(resolve => setTimeout(resolve, 100)); // Delay for demonstration
-        setLoading(false); // Set loading to false once the icon is loaded
-      } catch (error) {
-        console.error("Error loading icon:", error);
-        setLoading(false);
-      }
-    };
-
-    checkIcon();
-  }, [icon]); // Dependency array includes icon to re-check whenever the icon prop changes
+    const iconDefinition = findIconDefinition({ prefix: 'fas', iconName: icon as any });
+    if (!iconDefinition) {
+      console.error(`Icon ${icon} not found in FontAwesome library.`);
+      setIconExists(false);
+    }
+    setLoading(false);
+  }, [icon]);   
 
   if (loading) {
-    return <div>Loading...</div>; // Render a loading state or a spinner
+    return <div>Loading...</div>;
+  }
+
+  if (!iconExists) {
+    return <div>Icon not available</div>;
   }
 
   return <FontAwesomeIcon icon={icon as IconProp} color={color} size={size} />;
