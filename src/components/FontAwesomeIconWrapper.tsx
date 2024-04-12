a2e51212c7d312c13108a787a9bc9abbb9c57377
Šimon <simonbuchta2005@gmail.com>
2024-04-12 12:15:04 +0200

    error fixing

--- src/components/FontAwesomeIconWrapper.tsx
+++ src/components/FontAwesomeIconWrapper.tsx
@@ -3,7 +3,7 @@ import { IconProp, SizeProp, library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fal } from '@fortawesome/pro-light-svg-icons';
 import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { FC } from 'react';
+import { FC, memo } from 'react';
 library.add(fas, far, fal);
 
 interface FAWrapperProps {
@@ -12,9 +12,10 @@ interface FAWrapperProps {
   size?: SizeProp
 }
 
-
-const FontAwesomeIconWrapper: FC<FAWrapperProps> = ({ icon, color, size }) => {  
+const FontAwesomeIconWrapperComponent: FC<FAWrapperProps> = ({ icon, color, size }) => {
   return <FontAwesomeIcon icon={icon as IconProp} color={color} size={size} />;
 };
+
+const FontAwesomeIconWrapper = memo(FontAwesomeIconWrapperComponent);
 
 export default FontAwesomeIconWrapper;
