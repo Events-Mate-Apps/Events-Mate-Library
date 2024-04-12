2c548435d8753d11901083512fe7c3b3c35bf7f5
Šimon <simonbuchta2005@gmail.com>
2024-04-12 12:39:40 +0200

    test

--- src/components/FontAwesomeIconWrapper.tsx
+++ src/components/FontAwesomeIconWrapper.tsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp, SizeProp, library, findIconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  IconProp,
+  SizeProp,
+  library,
+  findIconDefinition,
+  IconPrefix // Importování typu IconPrefix
+} from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fal } from '@fortawesome/pro-light-svg-icons';
 import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { FC, memo } from 'react';
+import { FC } from 'react';
+
+// Přidání ikon do knihovny
 library.add(fas, far, fal);
 
 interface FAWrapperProps {
@@ -19,11 +27,18 @@ const FontAwesomeIconWrapper: FC<FAWrapperProps> = ({ icon, color, size }) => {
 
   useEffect(() => {
     const checkIconAvailability = () => {
-      const iconDefinition = findIconDefinition({ prefix: 'fas', iconName: icon as any });
+      // Explicitně deklarované prefixy jako IconPrefix[]
+      const prefixes: IconPrefix[] = ['fas', 'far', 'fal'];
+      let iconDefinition = null;
+      for (const prefix of prefixes) {
+        iconDefinition = findIconDefinition({ prefix, iconName: icon as any });
+        if (iconDefinition) break;
+      }
+      
       if (iconDefinition) {
         setIconAvailable(true);
         setLoading(false);
-      } else if (!loading) {
+      } else {
         setTimeout(checkIconAvailability, 100); // Re-check in 100ms
       }
     };
@@ -33,7 +48,7 @@ const FontAwesomeIconWrapper: FC<FAWrapperProps> = ({ icon, color, size }) => {
     checkIconAvailability();
 
     return () => {
-      setLoading(false); // Cleanup to prevent setting state after unmount
+      setLoading(false);
     };
   }, [icon]);
 
