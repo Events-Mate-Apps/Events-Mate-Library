7b61ea0fbb2b61b4677d34031e7a790a0d41724b
Šimon <simonbuchta2005@gmail.com>
2024-04-23 18:31:27 +0200

    spacing test

--- src/components/VendorDetail/image/EditableImageList.tsx
+++ src/components/VendorDetail/image/EditableImageList.tsx
@@ -63,6 +63,7 @@ const EditableImageList: React.FC<EditableImageListProps> = ({ vendor, setCurren
             <SimpleGrid
                 columns={4}
                 spacingY={{ sm: '20px', md: '35px', lg: '20px' }}
+                spacingX={{ sm: '20px', md: '35px', lg: '20px' }}
                 justifyContent="flex-start"
                 width="100%"
             >
