97fe496adf1e5fa360da977e21a0c9758f56d7d9
Šimon <simonbuchta2005@gmail.com>
2024-04-24 14:17:42 +0200

    width test

--- src/components/VendorDetail/image/EditableImageList.tsx
+++ src/components/VendorDetail/image/EditableImageList.tsx
@@ -61,6 +61,7 @@ const EditableImageList: React.FC<EditableImageListProps> = ({ vendor, setCurren
     return (
         <DndProvider backend={HTML5Backend}>
             <Wrap
+                width='100%'
                 spacing={{ sm: '10px', md: '20px' }}
                 justifyContent="flex-start"
             >
