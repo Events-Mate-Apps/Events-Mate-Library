47bcf52ba38a3152fb6b5ee39c4a701242d0aee7
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-05-28 08:12:09 +0200

    AsyncButton

---
+++ src/components/buttons/AsyncButton.tsx
@@ -0,0 +1,32 @@
+import { Button as ChakraButton, ButtonProps, forwardRef } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+
+interface AsyncButtonProps extends ButtonProps {
+  onClick: () => Promise<void>;
+}
+
+const AsyncButton: FC<AsyncButtonProps> = forwardRef(({ onClick, ...props }, ref) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      await onClick();
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <ChakraButton 
+      ref={ref}
+      onClick={handleClick}
+      isLoading={isLoading}
+      {...props}
+    >
+      {props.children}
+    </ChakraButton>
+  );
+});
+
+export default AsyncButton;
