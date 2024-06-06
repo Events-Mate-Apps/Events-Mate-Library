654546e8052f571622af8889a3ab402b9b9c674e
Honza Wawrzyk <honza.wawrzyk@seznam.cz>
2024-06-06 13:18:22 +0200

    profile change

--- src/components/admin/adminComponents/Profile.tsx
+++ src/components/admin/adminComponents/Profile.tsx
@@ -8,18 +8,16 @@ interface ProfileProps {
 }
 
 const Profile: FC<ProfileProps> = ({ user }) => {
-  const textColorPrimary = useColorModeValue('secondaryGray.900', 'white');
-  const textColorSecondary = 'gray.400';
 
   return (
     <Card mb="20px">
       <Flex align="center">
         <Avatar src={user.avatar} h="87px" w="87px" me="20px" />
         <Flex direction="column">
-          <Text color={textColorPrimary} fontWeight="bold" fontSize="2xl">
-            {user.name}
+          <Text color='gray.400' fontWeight="bold" fontSize="2xl">
+            {user.username}
           </Text>
-          <Text mt="1px" color={textColorSecondary} fontSize="md">
+          <Text mt="1px" color='gray.400' fontSize="md">
             {user.email}
           </Text>
         </Flex>
