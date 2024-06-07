c276889c66e27973e3e35c377d2dcbba5790b429
Honza Wawrzyk <honza.wawrzyk@seznam.cz>
2024-06-07 17:08:51 +0200

    add button

--- src/components/admin/adminComponents/Information.tsx
+++ src/components/admin/adminComponents/Information.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Flex, FormControl, SimpleGrid, Text, useColorModeValue, Input, Textarea, FormLabel, Box } from '@chakra-ui/react';
+import { Flex, FormControl, SimpleGrid, Text, useColorModeValue, Input, FormLabel, Box, Button } from '@chakra-ui/react';
 import Card from '../../card/Card';
 import useTranslation from 'next-translate/useTranslation';
 
@@ -16,32 +16,60 @@ const Information: FC<InformationProps> = (props) => {
   return (
     <FormControl>
       <Card mb="20px" {...rest}>
-        <Flex direction="column" mb="30px" ms="10px">
-          <Text fontSize="xl" color={textColorPrimary} fontWeight="bold">
+        <Flex 
+          direction="column" 
+          mb="30px"
+          ms="10px"
+        >
+          <Text 
+            fontSize="xl" 
+            color={textColorPrimary} 
+            fontWeight="bold"
+          >
             {t('user:settings.accountSettings')}
           </Text>
-          <Text fontSize="md" color={textColorSecondary}>
+          <Text 
+            fontSize="md" 
+            color={textColorSecondary}
+          >
             {t('user:settings.changeInformation')}
           </Text>
         </Flex>
-        <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={{ base: '20px', xl: '20px' }}>
-          <Box mb="0px" me="30px">
-            <FormLabel htmlFor="username">{t('common:username')}</FormLabel>
-            <Input id="username" placeholder="@simmmple.web" />
+        <SimpleGrid 
+          columns={{ sm: 1, md: 2 }} 
+          spacing={{ base: '20px', xl: '20px' }}
+        >
+          <Box  
+            mb="0px"   
+            me="30px" 
+          >
+            <FormLabel 
+              htmlFor="username"
+            >
+              {t('common:username')}
+            </FormLabel>
+            <Input 
+              id="username" 
+              placeholder="@simmmple.web" 
+            />
           </Box>
-          <Box mb="0px">
+          <Box>
             <FormLabel htmlFor="email">{t('common:emailAddress')}</FormLabel>
             <Input id="email" placeholder="mail@simmmple.com" />
           </Box>
           <Box mb="20px" me="30px">
             <FormLabel htmlFor="first_name">{t('guest:form.firstName')}</FormLabel>
-            <Input id="first_name" placeholder="John" />
+            <Input id="first_name" placeholder={t('guest:form.firstName')}/>
           </Box>
           <Box mb="20px">
             <FormLabel htmlFor="last_name">{t('guest:form.lastName')}</FormLabel>
-            <Input id="last_name" placeholder="William" />
+            <Input id="last_name" placeholder={t('guest:form.lastName')}/>
           </Box>
         </SimpleGrid>
+        <Button
+        >
+          {t('edit:saveChanges')}
+        </Button>
       </Card>
     </FormControl>
   );
