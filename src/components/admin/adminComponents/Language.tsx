5528c423784a2325dcd482c3ff733129e6adad4f
Honza Wawrzyk <honza.wawrzyk@seznam.cz>
2024-06-07 19:20:16 +0200

    commit

--- src/components/admin/adminComponents/Language.tsx
+++ src/components/admin/adminComponents/Language.tsx
@@ -1,39 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { Box, Heading, Text, Select, FormControl } from "@chakra-ui/react";
 import Card from '../../card/Card';
+import { api } from "~/utils/api";
 
 const LanguageSettings: FC = () => {
+  const [languages, setLanguages] = useState<string[]>([]);
+  const [currencies, setCurrencies] = useState<string[]>([]);
+
+  const fetchCurrencies = async () => {
+    try {
+      const { data } = await api.get('support/currencies');
+      setCurrencies(data);
+      console.log(data)
+    } catch (error) {
+      console.error("Error fetching currencies:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
   return (
     <FormControl>
-      <Card
-        maxW="md"
-        mx="auto"
-        mt={8}
-        p={6}
-        borderWidth={1}
-        borderRadius="lg"
-        boxShadow="lg"
-      >
+      <Card mb="20px">
         <Heading as="h2" size="lg" mb={4}>
           Account Settings
         </Heading>
         <Text mb={6}>Here you can change your account information</Text>
-        <Box mb={4} >
+        <Box mb={4}>
           <Text mb={2}>Language</Text>
           <Select placeholder="Select Language">
             <option value="english">English</option>
             <option value="spanish">Spanish</option>
             <option value="french">French</option>
-            {/* Add more languages as needed */}
           </Select>
         </Box>
         <Box mb={4}>
           <Text mb={2}>Currency</Text>
           <Select placeholder="Select Currency">
-            <option value="dollars">Dollars</option>
-            <option value="euros">Euros</option>
-            <option value="yen">Yen</option>
-            {/* Add more currencies as needed */}
+            {currencies.map((currency) => (
+              <option key={currency} value={currency}>{currency}</option>
+            ))}
           </Select>
         </Box>
       </Card>
