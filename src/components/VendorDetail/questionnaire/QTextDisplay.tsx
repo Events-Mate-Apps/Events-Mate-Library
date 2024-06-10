23b9b48b399bd0ae5fbcab1f1f880f76f650e236
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-10 19:19:25 +0200

    boolean

--- src/components/VendorDetail/questionnaire/QTextDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QTextDisplay.tsx
@@ -2,10 +2,13 @@ import { FC } from 'react';
 import { QuestionResponse, QuestionType } from '../../../interfaces/questionnaire';
 import { Box } from '@chakra-ui/react';
 import { useLocalization } from '../../../service/LocalizationService';
+import useTranslation from 'next-translate/useTranslation';
 
 
 const QTextDisplay: FC<{ response: QuestionResponse }> = ({ response }) => {
   const { getCurrentTranslation } = useLocalization()
+  const { t } = useTranslation()
+
   const content = (type: QuestionType) => {
     switch (type) {
       case QuestionType.TEXT:
@@ -13,13 +16,13 @@ const QTextDisplay: FC<{ response: QuestionResponse }> = ({ response }) => {
       case QuestionType.NUMERIC:
         return response.numericResponse
       case QuestionType.YES_NO:
-        return response.booleanResponse
+        return response.booleanResponse ? t('vendors:questionnaire.yes') : t('vendors:questionnaire.no')
     }
   }
 
   return (
     <Box p='10px'>
-      <p>{content(response.questionType)}</p>
+      <p>{(content(response.questionType))}</p>
     </Box>
   );
 }
