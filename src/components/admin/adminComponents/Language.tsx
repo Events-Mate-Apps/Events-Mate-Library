c611806df326f6db5c60e544256a5ba1878afa03
Honza-Wawrzyk <honza.wawrzyk@events-mate.com>
2024-06-11 10:52:57 +0200

    commit

--- src/components/admin/adminComponents/Language.tsx
+++ src/components/admin/adminComponents/Language.tsx
@@ -146,6 +146,11 @@ const LanguageSettings: FC = () => {
                 </option>
               ))}
             </Select>
+            <Select placeholder='Select option'>
+              <option value='option1'>Option 1</option>
+              <option value='option2'>Option 2</option>
+              <option value='option3'>Option 3</option>
+            </Select>
           </Box>
         </Flex>
         <Button mt={4} colorScheme="teal" onClick={handleSaveChanges}>
