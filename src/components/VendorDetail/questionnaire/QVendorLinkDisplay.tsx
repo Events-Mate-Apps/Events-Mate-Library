8cb99d2a39c68979f4be42e116c15f4fbea40d32
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-13 19:42:48 +0200

    vendor names in vendor link

--- src/components/VendorDetail/questionnaire/QVendorLinkDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QVendorLinkDisplay.tsx
@@ -1,12 +1,26 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { QDisplayComponentProps } from '../../../interfaces/questionnaire';
 import { Box } from '@chakra-ui/react';
+import { api } from '~/utils/api';
+import { SmallVendor } from '~/interfaces/vendor';
 
 const QLinkDisplay: FC<QDisplayComponentProps> = ({ responses }) => {
+  const [vendors, setVendors] = useState<SmallVendor[]>([])
+
+  const getVendors = async () => {
+    responses[0].partnerVendorIds.forEach(async id => {
+      const { data: vendor } = await api.get(`smallVendors/${id}`)
+      setVendors(prevState => [vendor, ...prevState])
+    })
+  }
+
+  useEffect(() => {
+    getVendors()
+  }, [])
 
   return (
     <Box p='10px'>
-      <p>{JSON.stringify(responses[0].partnerVendorIds)}</p>
+      <p>{JSON.stringify(vendors.map(e => e.name))}</p>
     </Box>
   );
 }
