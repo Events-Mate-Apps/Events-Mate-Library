a46c58d6ee79f60deb9d62eacf924cca482ebcd4
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-13 19:45:34 +0200

    partnerVendorIds display fixes

--- src/components/VendorDetail/questionnaire/QVendorLinkDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QVendorLinkDisplay.tsx
@@ -1,16 +1,22 @@
 import { FC, useEffect, useState } from 'react';
 import { QDisplayComponentProps } from '../../../interfaces/questionnaire';
 import { Box } from '@chakra-ui/react';
-import { api } from '~/utils/api';
-import { SmallVendor } from '~/interfaces/vendor';
+import { api } from '../../../utils/api';
+import { Vendor } from '../../../interfaces/vendor';
+import { useNotification } from '../../../service/NotificationService';
 
 const QLinkDisplay: FC<QDisplayComponentProps> = ({ responses }) => {
-  const [vendors, setVendors] = useState<SmallVendor[]>([])
+  const [vendors, setVendors] = useState<Vendor[]>([])
+  const { showError } = useNotification()
 
   const getVendors = async () => {
     responses[0].partnerVendorIds.forEach(async id => {
-      const { data: vendor } = await api.get(`smallVendors/${id}`)
-      setVendors(prevState => [vendor, ...prevState])
+      try {
+        const { data: vendor } = await api.get(`vendors/${id}`)
+        setVendors(prevState => [vendor, ...prevState])
+      } catch (error) {
+        showError({ error })
+      }
     })
   }
 
