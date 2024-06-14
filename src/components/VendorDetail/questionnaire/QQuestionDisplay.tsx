68b36a4bee897c1e072066eb84317a84a71b50b4
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-14 18:37:19 +0200

    layout fixes

--- src/components/VendorDetail/questionnaire/QQuestionDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QQuestionDisplay.tsx
@@ -37,17 +37,17 @@ const QQuestionDisplay: FC<{ question: Question, fsTitle: number, fsDesc: number
                 </Text>
               )}
             </Box>
-            {question.responses && ResponseComponent === QTextDisplay ? <ResponseComponent 
+            {ResponseComponent === QTextDisplay && (question.responses ? <ResponseComponent 
               responses={question.responses}
               options={question.options} 
               fsTitle={fsTitle - 2}
-            /> : 'No Responses'}
+            /> : 'No Responses')}
           </AccordionButton>
-          {question.responses && ResponseComponent !== QTextDisplay ? <ResponseComponent 
+          {ResponseComponent !== QTextDisplay && (question.responses ? <ResponseComponent 
             responses={question.responses}
             options={question.options} 
             fsTitle={fsTitle - 2}
-          /> : 'No Responses'}
+          /> : 'No Responses')}
         </Box>
         {(question.subQuestions && question.subQuestions[0]) && question.subQuestions.map((q) => (
           <AccordionPanel key={q.id}>
