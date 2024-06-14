550755f10f6b21b0dae8565584389b7f61267f53
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-14 11:30:40 +0200

    accordion test vol 2

--- src/components/VendorDetail/questionnaire/QQuestionDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QQuestionDisplay.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Question } from '../../../interfaces/questionnaire';
-import { Accordion, AccordionButton, AccordionIcon, AccordionPanel, Box, Divider, Text, useColorModeValue } from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Divider, Text, useColorModeValue } from '@chakra-ui/react';
 import { useLocalization } from '../../../service/LocalizationService';  
 import { QUESTION_DISPLAY_COMPONENT } from '../../../constants/questionnaire';
 import QTextDisplay from './QTextDisplay';
@@ -14,43 +14,45 @@ const QQuestionDisplay: FC<{ question: Question, fsTitle: number, fsDesc: number
 
   return (
     <Accordion py='10px' pl='10px'>
-      <Box 
-        display={ResponseComponent === QTextDisplay ? 'flex' : 'block'} 
-        alignItems='center'
-        justifyContent='space-between'
-      >
-        <AccordionButton>
-          <AccordionIcon />
-          <Box>
-            {(question.titleContent && question.titleContent.translations) && (
-              <Text color={textColor} fontSize={`${fsTitle}px`} fontWeight='700'>
-                {getCurrentTranslation(question.titleContent)}
-              </Text>
-            )}
-            {(question.descriptionContent && question.descriptionContent.translations) && (
-              <Text color={secondaryTextColor} fontSize={`${fsDesc}px`} fontWeight='500'>
-                {getCurrentTranslation(question.descriptionContent)}
-              </Text>
-            )}
-          </Box>
-          {question.responses ? <ResponseComponent 
-            responses={question.responses}
-            options={question.options} 
-            fsTitle={fsTitle - 2}
-          /> : 'No Responses'}
-        </AccordionButton>
-      </Box>
-      <Divider borderColor='darkgray' h='1px' />
-      <AccordionPanel>
-        {question.subQuestions && question.subQuestions.map((q) => (
-          <QQuestionDisplay
-            fsTitle={fsTitle - 2}
-            fsDesc={fsDesc - 2}
-            question={q}
-            key={q.id}
-          />
-        ))}
-      </AccordionPanel>
+      <AccordionItem>
+        <Box 
+          display={ResponseComponent === QTextDisplay ? 'flex' : 'block'} 
+          alignItems='center'
+          justifyContent='space-between'
+        >
+          <AccordionButton>
+            <AccordionIcon />
+            <Box>
+              {(question.titleContent && question.titleContent.translations) && (
+                <Text color={textColor} fontSize={`${fsTitle}px`} fontWeight='700'>
+                  {getCurrentTranslation(question.titleContent)}
+                </Text>
+              )}
+              {(question.descriptionContent && question.descriptionContent.translations) && (
+                <Text color={secondaryTextColor} fontSize={`${fsDesc}px`} fontWeight='500'>
+                  {getCurrentTranslation(question.descriptionContent)}
+                </Text>
+              )}
+            </Box>
+            {question.responses ? <ResponseComponent 
+              responses={question.responses}
+              options={question.options} 
+              fsTitle={fsTitle - 2}
+            /> : 'No Responses'}
+          </AccordionButton>
+        </Box>
+        <Divider borderColor='darkgray' h='1px' />
+        <AccordionPanel>
+          {question.subQuestions && question.subQuestions.map((q) => (
+            <QQuestionDisplay
+              fsTitle={fsTitle - 2}
+              fsDesc={fsDesc - 2}
+              question={q}
+              key={q.id}
+            />
+          ))}
+        </AccordionPanel>
+      </AccordionItem>
     </Accordion>
   );
 }
