354f24c4e9ccc07903653996a157404df89eb0a8
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-17 13:28:59 +0200

    price entry test

--- src/components/VendorDetail/questionnaire/QPriceEntryDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QPriceEntryDisplay.tsx
@@ -1,12 +1,36 @@
 import { FC } from 'react';
 import { QDisplayComponentProps } from '../../../interfaces/questionnaire';
-import { Box } from '@chakra-ui/react';
+import { Box, Divider, Text, useColorModeValue } from '@chakra-ui/react';
 
 const QPriceDisplay: FC<QDisplayComponentProps> = ({ responses }) => {
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
+  const secondaryTextColor = useColorModeValue('secondaryGray.700', 'white');
 
   return (
     <Box p='10px'>
-      <p>{JSON.stringify(responses[0].plans)}</p>
+      <Text color={textColor} fontSize={`${14}px`} fontWeight='700'>
+          Coca-Cola
+      </Text>
+      <Text color={secondaryTextColor} fontSize={`${12}px`} fontWeight='700'>
+          Coca-Cola Popisek
+      </Text>
+      <p>12 Ks / 300 CZK</p>
+      <Divider></Divider>
+      <Text color={textColor} fontSize={`${14}px`} fontWeight='700'>
+          Fanta
+      </Text>
+      <Text color={secondaryTextColor} fontSize={`${12}px`} fontWeight='700'>
+          Fanta Popisek
+      </Text>
+      <p>12 Ks / 300 CZK</p>
+      <Divider></Divider>
+      <Text color={textColor} fontSize={`${14}px`} fontWeight='700'>
+          Sprite
+      </Text>
+      <Text color={secondaryTextColor} fontSize={`${12}px`} fontWeight='700'>
+          Sprite
+      </Text>
+      <p>12 Ks / 300 CZK</p>
     </Box>
   );
 }
