405ee3b6eef8a5da72c1cff20861ce07fc8177db
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-20 20:02:28 +0200

    debugging

--- src/components/VendorDetail/questionnaire/QPlan.tsx
+++ src/components/VendorDetail/questionnaire/QPlan.tsx
@@ -11,26 +11,30 @@ const QPlan: FC<QPlanProps> = ({ plan }) => {
   const textColor = useColorModeValue('secondaryGray.900', 'white');
   const secondaryTextColor = useColorModeValue('secondaryGray.700', 'white');
 
-  const { getCurrentTranslation, getCurrentPrice } = useLocalization()
-  console.log(getCurrentPrice(plan.prices))
+  const { getCurrentTranslation, getCurrentPrice } = useLocalization();
+  const currentPrice = getCurrentPrice(plan.prices);
+
   return (
     <Flex p='10px' justifyContent='space-between'>
       <Box>
-        {plan.titleContent.translations && <Text color={textColor} fontSize={`${14}px`} fontWeight='700'>
-          {getCurrentTranslation(plan.titleContent)}
-        </Text>}
-        {plan.descriptionContent.translations && <Text color={secondaryTextColor} fontSize={`${12}px`} fontWeight='700'>
-          {getCurrentTranslation(plan.descriptionContent)}
-        </Text>}
+        {plan.titleContent.translations && (
+          <Text color={textColor} fontSize='14px' fontWeight='700'>
+            {getCurrentTranslation(plan.titleContent)}
+          </Text>
+        )}
+        {plan.descriptionContent.translations && (
+          <Text color={secondaryTextColor} fontSize='12px' fontWeight='700'>
+            {getCurrentTranslation(plan.descriptionContent)}
+          </Text>
+        )}
       </Box>
       <Text fontSize='2xl'>
-        {plan.unitVolume} 
-        {plan.unit} 
-        / 
-        {getCurrentPrice(plan.prices).amount ? getCurrentPrice(plan.prices).amount : 'No amount :P'} 
-        {getCurrentPrice(plan.prices).currencyISO ? getCurrentPrice(plan.prices).currencyISO : 'No ISO :P'}</Text>
+        {plan.unitVolume} {plan.unit} /{' '}
+        {currentPrice?.amount ? currentPrice.amount : 'No amount :P'}{' '}
+        {currentPrice?.currencyISO ? currentPrice.currencyISO : 'No ISO :P'}
+      </Text>
     </Flex>
   );
-}
-  
+};
+
 export default QPlan;
