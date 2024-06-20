28c97a92018f1a9f5bdce4bc04a5e061c141b469
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-20 23:51:18 +0200

    layout test

--- src/components/VendorDetail/questionnaire/QSectionDisplay.tsx
+++ src/components/VendorDetail/questionnaire/QSectionDisplay.tsx
@@ -15,7 +15,7 @@ const QSectionDisplay: FC<{ section: Section, isLast: boolean }> = ({ section, i
       pt='10xp'
       mb='60px'
       w={{ base: '100%' }}
-      borderBottom={`${isLast ? 0 : 1}px`}
+      borderTop={`${isLast ? 0 : 1}px`}
       borderColor='chakra-border-color._dark'
     >
       {(section.titleContent && section.titleContent.translations) && <Text color={textColor} fontSize='20px' fontWeight='700'>
