c918aa1e3300f4579a5a218cad2619c3b8c9236e
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-06-20 23:33:12 +0200

    layout test

--- src/components/VendorDetail/questionnaire/QuestionnaireResponsesContainer.tsx
+++ src/components/VendorDetail/questionnaire/QuestionnaireResponsesContainer.tsx
@@ -1,33 +1,42 @@
 import { FC } from 'react';
 import { Questionnaire } from '../../../interfaces/questionnaire';
-import { Box, SimpleGrid, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
 import { useLocalization } from '../../../service/LocalizationService';
 import QSectionDisplay from './QSectionDisplay';
-  
-
 
 const QuestionnaireResponsesContainer: FC<{ questionnaire: Questionnaire }> = ({ questionnaire }) => {
-  const { getCurrentTranslation } = useLocalization()
+  const { getCurrentTranslation } = useLocalization();
   const textColor = useColorModeValue('secondaryGray.900', 'white');
 
   return (
     <Box p='10px'>
-      {(questionnaire.titleContent && questionnaire.titleContent.translations) && <Text color={textColor} fontSize='22px' fontWeight='700'>
-        {getCurrentTranslation(questionnaire.titleContent)}
-      </Text>}
-      {(questionnaire.descriptionContent && questionnaire.descriptionContent.translations) && <Text color={textColor} fontSize='22px' fontWeight='700'>
-        {getCurrentTranslation(questionnaire.descriptionContent)}
-      </Text>}
-      <SimpleGrid columns={{ base: 1, lg: 2 }}>
+      {(questionnaire.titleContent && questionnaire.titleContent.translations) && (
+        <Text color={textColor} fontSize='22px' fontWeight='700'>
+          {getCurrentTranslation(questionnaire.titleContent)}
+        </Text>
+      )}
+      {(questionnaire.descriptionContent && questionnaire.descriptionContent.translations) && (
+        <Text color={textColor} fontSize='22px' fontWeight='700'>
+          {getCurrentTranslation(questionnaire.descriptionContent)}
+        </Text>
+      )}
+      <Flex wrap="wrap">
         {questionnaire.sections.map((section, index) => (
-          <QSectionDisplay
-            section={section}
+          <Box
             key={section.id}
-            isLast={questionnaire.sections.length - 1 === index}
-          />))}
-      </SimpleGrid>
+            flex={{ base: '0 0 100%', lg: '0 0 50%' }}
+            maxW={{ base: '100%', lg: '50%' }}
+            p='10px'
+          >
+            <QSectionDisplay
+              section={section}
+              isLast={questionnaire.sections.length - 1 === index}
+            />
+          </Box>
+        ))}
+      </Flex>
     </Box>
   );
-}
-  
+};
+
 export default QuestionnaireResponsesContainer;
