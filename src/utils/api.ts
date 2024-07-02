a88e89db9ceff1bd833559aa1bdb0828e89bbad0
Honza-Wawrzyk <honza.wawrzyk@events-mate.com>
2024-07-02 20:12:16 +0200

    dffd

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,7 +5,6 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const getToken = (): string | undefined => {
   const user = store.get('user')
-  if (!user || !user.token) return 
   return user.token.secret
 } 
 
