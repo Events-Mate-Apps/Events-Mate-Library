df7dccf9dd5e196754bf176f7689fed5d855f770
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-07-06 14:44:37 +0200

    small fixes

--- src/stores/notification.ts
+++ src/stores/notification.ts
@@ -8,7 +8,7 @@ import { getNotificationMessage } from './helpers/notification';
 type AlertStatus = 'info' | 'success' | 'error' | 'warn'
 
 interface Options {
-  title?: string;
+  title: string;
   description?: string;
   duration?: number;
   position?: ToastPosition;
@@ -55,47 +55,39 @@ const useNotificationStore = create<NotificationState>((set, get) => {
       }) ,{ autoClose: options.duration || 5000, });
     },
     showCustomError: async (options: Options) => {
-      const t = await getT(get().locale, 'notification')
-
       console.error(`${options.title}: ${options.description}`);
       toast.error( getNotificationMessage({
-        title: `${t('notification:error')}`,
+        title: options.title,
         description: options.description,
       }) ,{ autoClose: options.duration, });
     },
     showSuccess: async (options?: Options) => {
-      const t = await getT(get().locale, 'notification')
-
       if (!options) {
         get().plainToast('success');
         return;
       }
       toast.success( getNotificationMessage({
-        title: `${t('notification:success')}`,
+        title: options.title,
         description: options.description
       }) ,{ autoClose: options.duration, });
     },
     showWarning: async (options?: Options) => {
-      const t = await getT(get().locale, 'notification')
-
       if (!options) {
         get().plainToast('warn');
         return;
       }
       toast.warn( getNotificationMessage({
-        title: `${t('notification:warn')}`,
+        title: options.title,
         description: options.description
       }) ,{ autoClose: options.duration, });
     },
     showInfo: async (options?: Options) => {
-      const t = await getT(get().locale, 'notification')
-
       if (!options) {
         get().plainToast('info');
         return;
       }
       toast.info( getNotificationMessage({
-        title: `${t('notification:info')}`,
+        title: options.title,
         description: options.description
       }) ,{ autoClose: options.duration, });
     },
