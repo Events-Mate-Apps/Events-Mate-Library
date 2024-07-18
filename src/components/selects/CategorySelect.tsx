6c17a5c03be6ca01ba21bde00664acdb0829ffc7
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-07-18 13:18:21 +0200

    initial commit

---
+++ src/components/selects/CategorySelect.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, FC } from 'react';
+// import { Select } from 'chakra-react-select';
+
+import useNotificationStore from '../../stores/notification'
+import { api } from '~/utils/api';
+import { Categories, SelectCategory } from '~/interfaces/category';
+import { useLocalization } from '~/service/LocalizationService';
+
+interface CategorySelectProps {
+  defaultValue?: string;
+  name: string;
+  error?: any;
+}
+
+export const CategorySelect: FC<CategorySelectProps> = ({
+  defaultValue
+}) => {
+  const [categories, setCategories] = useState<SelectCategory[]>([]);
+
+  const { showError } = useNotificationStore()
+  const { getCurrentTranslation } = useLocalization()
+
+  useEffect(() => {
+    fetchCategories()
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const { data: cats } = await api.get<Categories>('vendors/categories/vendor-categories')
+
+      const selectCategories: SelectCategory[] = cats.map((e) => ({
+        title: getCurrentTranslation(e.titleContent),
+        value: e.id
+      }))
+
+      setCategories(selectCategories)
+    } catch (error) {
+      showError({ error })
+    }
+  }
+
+  return <>
+    {categories.map((e) => 
+      <p key={e.value}>title: {e.title}, value: {e.value}</p>
+    )}
+  </>
+}
