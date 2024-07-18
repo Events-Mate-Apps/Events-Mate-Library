23b627692b44ba7b6b9d51880435848f3793dac9
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-07-18 13:38:07 +0200

    label

--- src/components/selects/CategorySelect.tsx
+++ src/components/selects/CategorySelect.tsx
@@ -8,6 +8,7 @@ import { useLocalization } from '../../service/LocalizationService';
 import { Controller, useFormContext } from 'react-hook-form';
 import { Select } from 'chakra-react-select';
 import useTranslation from 'next-translate/useTranslation';
+import { FormLabel, useColorModeValue } from '@chakra-ui/react';
 
 interface CategorySelectProps {
   defaultValue?: string;
@@ -27,6 +28,7 @@ export const CategorySelect: FC<CategorySelectProps> = ({
   const { showError } = useNotificationStore()
   const { getCurrentTranslation } = useLocalization()
   const { t } = useTranslation()
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
 
   useEffect(() => {
     fetchCategories()
@@ -48,6 +50,18 @@ export const CategorySelect: FC<CategorySelectProps> = ({
   }
 
   return <>
+    <FormLabel
+      textAlign="start"
+      marginEnd='3'
+      marginBottom='2'
+      display="flex"
+      marginLeft='2'
+      fontSize="sm"
+      color={textColor}
+      fontWeight="bold"
+    >
+      {t('common:category')}
+    </FormLabel>
     <Controller
       control={control}
       name="category"
