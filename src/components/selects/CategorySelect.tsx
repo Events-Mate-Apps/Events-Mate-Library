c4ac3767dee3c94c4e7d23af360c56de518e417b
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-07-19 11:49:01 +0200

    test

--- src/components/selects/CategorySelect.tsx
+++ src/components/selects/CategorySelect.tsx
@@ -67,6 +67,7 @@ const CategorySelect: FC<CategorySelectProps> = ({ defaultValue, name }) => {
             }))}
             getOptionLabel={(option) => option.label}
             getOptionValue={(option) => option.value.id}
+            onChange={(selectedOption) => field.onChange(selectedOption?.value)}
           />
         )}
       />
