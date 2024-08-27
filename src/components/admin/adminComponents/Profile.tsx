993fa4036a27c8fb0fa1bed4bbf44cecf6993e40
Honza-Wawrzyk <honza.wawrzyk@events-mate.com>
2024-08-28 00:39:21 +0200

    initial commit

--- src/components/admin/adminComponents/Profile.tsx
+++ src/components/admin/adminComponents/Profile.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Avatar, Card } from '@chakra-ui/react';
+import { Flex, Text, Avatar, Card, useColorModeValue } from '@chakra-ui/react';
 import { FC } from 'react';
 import { UserData } from '../../../interfaces/user';
 
@@ -7,17 +7,18 @@ interface ProfileProps {
 }
 
 const Profile: FC<ProfileProps> = ({ user }) => {
-
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
+ 
 
   return (
     <Card mb="20px">
       <Flex align="center">
         <Avatar src={user.username} h="87px" w="87px" me="20px" />
         <Flex direction="column">
-          <Text color='#1B2559' fontWeight="bold" fontSize="2xl">
+          <Text color={textColor} fontWeight="bold" fontSize="2xl">
             {user.username}
           </Text>
-          <Text mt="1px" color='#1B2559' fontSize="md">
+          <Text mt="1px" color={textColor} fontSize="md">
             {user.email}
           </Text>
         </Flex>
