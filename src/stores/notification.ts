2d12a64bf8c17339cdb8ea528e6e8da90149bf07
SimonBuchtaEM <simon.buchta@events-mate.com>
2024-08-29 13:16:17 +0200

    test

--- src/stores/notification.ts
+++ src/stores/notification.ts
@@ -48,7 +48,7 @@ const useNotificationStore = create<NotificationState>((set, get) => {
 
       const err = options.error as CustomError;
       console.error(err.message || err.raw?.message);
-      console.log('err:', JSON.stringify(err))
+      console.log('err:', err.response.data.message)
       toast.error( getNotificationMessage({
         title: `${t('error')}`,
         description: `${t('error')}: ${err.message || err.raw?.message}`,
