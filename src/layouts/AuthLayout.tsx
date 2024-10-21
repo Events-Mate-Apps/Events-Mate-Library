53ebcfb76b9968e4538385962b9920f428d9c15b
Honza-Wawrzyk <honza.wawrzyk@seznam.cz>
2024-10-21 19:45:42 +0200

    Auth

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Text, useColorModeValue, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Heading, Text, useColorModeValue, useBreakpointValue, HStack } from '@chakra-ui/react';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
@@ -43,16 +43,23 @@ const AuthLayout: FC<AuthLayoutProps> = ({ form, animatedContent, isEventsMate }
 
   return (
     <Flex h="100vh" position="relative">
-      <Flex
+      <HStack
         position="absolute"
         bottom="4"
         right="4"
+        spacing="4"
         zIndex="10"
-        flexDir="row"
+        sx={{
+          '.chakra-button': {
+            position: 'static !important',
+            right: 'auto !important',
+            bottom: 'auto !important',
+          },
+        }}
       >
         <LanguageSelect />
         <ThemeSwitch isEventsMate={isEventsMate} isOnGradient={isAnimationVisible} />
-      </Flex>
+      </HStack>
 
       <Flex 
         w={isAnimationVisible ? '50%' : '100%'} 
@@ -77,6 +84,7 @@ const AuthLayout: FC<AuthLayoutProps> = ({ form, animatedContent, isEventsMate }
         </Flex>
       </Flex>
 
+      {/* Animated Content */}
       {isAnimationVisible && (
         <Flex 
           w="50%" 
