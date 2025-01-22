d7635d868b5d33a0b67146a4e83c2f46ee21ce8f
Honza-Wawrzyk <honza.wawrzyk@seznam.cz>
2025-01-22 12:24:10 +0100

    apits turns snakecase to camelcase

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
 export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
@@ -13,6 +13,27 @@ const instance: AxiosInstance = axios.create({
   },
 });
 
+function toCamelCase(key: string): string {
+  return key.replace(/([-_][a-z])/gi, ($1) =>
+    $1.toUpperCase().replace('_', '').replace('-', ''),
+  );
+}
+
+instance.interceptors.response.use((response: AxiosResponse) => {
+  if (response.data && typeof response.data === 'object') {
+    const camelCasedData = Object.fromEntries(
+      Object.entries(response.data).map(([key, value]) => [
+        toCamelCase(key),
+        value,
+      ])
+    );
+    return {
+      ...response,
+      data: camelCasedData,
+    };
+  }
+  return response;
+});
 
 export const setAuthTokenHeader = (token: string | null) => {
   if (token) {
@@ -21,7 +42,7 @@ export const setAuthTokenHeader = (token: string | null) => {
 };
 
 export const resetAuthTokenHeader = () => {
-  instance.defaults.headers.common['Authorization'] = null
+  instance.defaults.headers.common['Authorization'] = null;
 };
 
 export const api = instance;
