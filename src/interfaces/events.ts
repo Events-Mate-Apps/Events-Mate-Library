fe7194f6774d14560654118dcbdc9f09c3152aac
Honza-Wawrzyk <honza.wawrzyk@seznam.cz>
2025-02-07 10:27:02 +0100

    events interface

---
+++ src/interfaces/events.ts
@@ -0,0 +1,42 @@
+export interface LocalizedContent {
+  cz: string;
+  en: string;
+  fr: string;
+}
+  
+export interface ColorScheme {
+  primary: string;
+  secondary: string;
+}
+  
+export interface Event {
+  eventType: number;
+  descriptionShort: LocalizedContent;
+  description: LocalizedContent;
+  dressCodeDesc: LocalizedContent;
+  guestsInfoLocEntityId: LocalizedContent;
+  colorScheme: ColorScheme;
+  preferredLanguage: string;
+  datetimeFrom: string;
+  datetimeTo: string;
+  justDate: boolean;
+  pinInfo: string;
+}
+  
+export interface EventResponse {
+  id: string;
+  event: Event;
+}
+  
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+  error?: string;
+}
+  
+export interface CreateEventRequest extends Event {}
+  
+export interface GetEventResponse extends ApiResponse<Event> {}
+  
+export interface GetEventsResponse extends ApiResponse<Event[]> {}
+  
